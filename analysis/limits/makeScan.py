dac48db19246d0945ae89098fdfcfbf445d9ad87
Nick Amin <amin.nj@gmail.com>
2017-06-19 13:40:43 -0700

    option to not redo the card when making the NLL scan

--- analysis/limits/makeScan.py
+++ analysis/limits/makeScan.py
@@ -10,26 +10,28 @@ import os
 # cardname =  "card_tttt_asimov.txt"
 # # createCard.writeOneCard(thedir,cardname, do_expected_data=True)
 
-def make_scan(thedir):
+def make_scan(thedir, noredocard=False):
     cardname =  "card_tttt_asimov.txt"
     # thedir, cardname = fullcardname.rsplit("/",1)
-    createCard.writeOneCard(thedir,cardname, do_expected_data=True)
+    if not noredocard:
+        createCard.writeOneCard(thedir,cardname, do_expected_data=True)
     # os.system("combine  -M MultiDimFit --algo grid --setPhysicsModelParameterRanges r=0,8.5 --redefineSignalPOI r {0} ; python plot1DScan.py  higgsCombineTest.MultiDimFit.mH120.root --main-label Expected ".format(thedir+"/"+cardname))
     fullcard = thedir+"/"+cardname
     cmd = "combine  -M MultiDimFit --algo grid --setPhysicsModelParameterRanges r=0,8.5 --saveFitResult --redefineSignalPOI r {0} ; python ../limits/plot1DScan.py  higgsCombineTest.MultiDimFit.mH120.root --main-label Expected ".format(fullcard)
     print "[!] Running: {0}".format(cmd)
     os.system(cmd)
+    print "made {0}/scan.pdf".format(thedir)
 
     cmd = "combine -M MaxLikelihoodFit {0} --saveShapes --saveWithUncertainties -n name".format(fullcard)
     print "[!] Running: {0}".format(cmd)
     os.system(cmd)
     # os.system("ic scan.pdf")
-    print "made {0}/scan.pdf".format(thedir)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", help="directory")
+    parser.add_argument("-N", "--noredocard", help="*don't* remake the card", action="store_true")
     args = parser.parse_args()
 
-    make_scan(args.dir)
+    make_scan(args.dir, args.noredocard)
