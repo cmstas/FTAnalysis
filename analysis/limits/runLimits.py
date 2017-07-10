3ceeb665c5729c7a4c5768703af6cda4cb28fabc
Nick Amin <amin.nj@gmail.com>
2017-07-10 13:52:33 -0700

    observed and expected

--- analysis/limits/runLimits.py
+++ analysis/limits/runLimits.py
@@ -42,19 +42,20 @@ def parse_lims(lim_lines, fb=False):
             "mu_down": d.get("mu_down",-1.),
             }
 
-def print_lims(d_lims, fb=False):
+def print_lims(d_lims, fb=False, unblinded=False):
     unit = "pb" if not fb else "fb"
-    # print "  Obs: %.2f %s" % (d_lims["obs"], unit)
+    if unblinded:
+        print "  Obs UL: %.2f %s" % (d_lims["obs"], unit)
     print "  Exp UL: %.2f +%.2f -%.2f %s" % (d_lims["exp"], d_lims["sp1"]-d_lims["exp"], d_lims["exp"]-d_lims["sm1"], unit)
     if d_lims.get("significance",-1) > 0.:
-        print "  Exp sig: %.3f (p-value: %.3f)" % (d_lims["significance"], d_lims["pvalue"])
+        print "  Sig: %.3f (p-value: %.3f)" % (d_lims["significance"], d_lims["pvalue"])
     if d_lims.get("mu",-1) > 0.:
-        print "  Exp mu: %.3f (+%.3f -%.3f)" % (d_lims["mu"], d_lims["mu_up"], d_lims["mu_down"])
+        print "  Mu: %.3f (+%.3f -%.3f)" % (d_lims["mu"], d_lims["mu_up"], d_lims["mu_down"])
 
-def get_lims(card, regions="srcr", redocard=True, redolimits=True, domcfakes=False, verbose=True, dolimits=True, dosignificance=True, doscan=True):
+def get_lims(card, regions="srcr", redocard=True, redolimits=True, domcfakes=False, verbose=True, dolimits=True, dosignificance=True, doscan=True, unblinded=False,sig="tttt"):
 
     if ".txt" not in card:
-        card += "/card_tttt_{0}.txt".format(regions)
+        card += "/card_{0}_{1}.txt".format(sig, regions)
         if verbose: print ">>> [!] no card name specified, so using {0}".format(card)
 
     full_card_name = "{0}".format(card)
@@ -63,30 +64,35 @@ def get_lims(card, regions="srcr", redocard=True, redolimits=True, domcfakes=Fal
     if not os.path.isfile(full_card_name) or redocard:
         if verbose: print ">>> Making card"
         dirname, cardname = card.rsplit("/",1)
-        createCard.writeOneCard(dirname,cardname, kine=regions,domcfakes=domcfakes)
+        createCard.writeOneCard(dirname,cardname, kine=regions,domcfakes=domcfakes, signal=sig)
     else:
         if verbose: print ">>> [!] Card already made, so reusing. Pass the --redocard flag to remake the card"
 
     if not dolimits: return {}
 
     if not os.path.isfile(full_log_name) or (redolimits or redocard):
-        limit_cmd = "combine -M Asymptotic {0} --noFitAsimov  2>&1 | tee {1}".format(full_card_name, full_log_name)
+        # extra = "--noFitAsimov" if not unblinded else ""
+        extra = "--noFitAsimov"
+        limit_cmd = "combine -M Asymptotic {0} {1}  2>&1 | tee {2}".format(full_card_name, extra, full_log_name)
         if verbose: print ">>> Running combine [{0}]".format(limit_cmd)
         stat, out = commands.getstatusoutput(limit_cmd)
         if dosignificance:
-            significance_cmd = "combine -M ProfileLikelihood {0} --significance -t -1 --expectSignal=1  2>&1 | tee -a {1}".format(full_card_name, full_log_name)
-            if verbose: print ">>> Running combine for EXPECTED significance [{0}]".format(significance_cmd)
+            extra = "-t -1 --expectSignal=1" if not unblinded else ""
+            significance_cmd = "combine -M ProfileLikelihood {0} --significance {1}  2>&1 | tee -a {2}".format(full_card_name, extra, full_log_name)
+            if verbose: print ">>> Running combine for significance [{0}]".format(significance_cmd)
             stat, out_sig = commands.getstatusoutput(significance_cmd)
             out += out_sig
         if doscan:
             dirname, cardname = card.rsplit("/",1)
-            do_blind=True
+            do_blind=not unblinded
+            cardname_scan = cardname
+            full_card_name_scan = full_card_name
             if do_blind:
                 cardname_scan = cardname.replace(".txt","_asimov.txt")
                 full_card_name_scan = full_card_name.replace(".txt","_asimov.txt")
-            createCard.writeOneCard(dirname,cardname_scan, do_expected_data=do_blind, kine=regions,domcfakes=domcfakes)
+            createCard.writeOneCard(dirname,cardname_scan, do_expected_data=do_blind, kine=regions,domcfakes=domcfakes, signal=sig)
             scan_cmd = "combine -M MaxLikelihoodFit {0} --saveShapes --saveWithUncertainties -n name 2>&1 | tee -a {1}".format(full_card_name_scan, full_log_name)
-            if verbose: print ">>> Running combine for EXPECTED mu [{0}]".format(scan_cmd)
+            if verbose: print ">>> Running combine for mu [{0}]".format(scan_cmd)
             stat, out_scan = commands.getstatusoutput(scan_cmd)
             out += out_scan
     else:
@@ -110,17 +116,25 @@ if __name__ == "__main__":
     parser.add_argument("-C", "--redocard", help="force the recreation of the card", action="store_true")
     parser.add_argument("-L", "--redolimits", help="force the rerunning of limits", action="store_true")
     parser.add_argument("-M", "--domcfakes", help="use fakes from MC", action="store_true")
+    parser.add_argument("-X", "--noscan", help="don't do a likelihood fit/scan", action="store_true")
+    parser.add_argument("-Y", "--nosignificance", help="don't calculate significance", action="store_true")
     parser.add_argument("-r", "--regions", help="srcr or disc for SR+CR limits or BDT limits", default="srcr")
+    parser.add_argument("-u", "--unblinded", help="show unblinded quantities", action="store_true")
+    parser.add_argument("-s", "--sig", help="signal name", default="tttt")
     args = parser.parse_args()
 
     # d_lims = get_lims(args)
     d_lims = get_lims(
             card=args.card,
+            sig=args.sig,
             regions=args.regions,
             redocard=args.redocard,
             redolimits=args.redolimits,
             domcfakes=args.domcfakes,
+            unblinded=args.unblinded,
+            doscan=(not args.noscan),
+            dosignificance=(not args.nosignificance),
             )
     print "------------------------------"
-    print_lims(d_lims, fb=True)
+    print_lims(d_lims, fb=True, unblinded=args.unblinded)
     print "------------------------------"
