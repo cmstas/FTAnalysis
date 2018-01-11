bb23449cb1ce7f195673d078192da99b759c8f22
Caleb Fangmeier <caleb@fangmeier.tech>
2018-01-11 10:08:04 -0800

    A bit of refactoring of setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 therelease=CMSSW_7_4_7_patch1
-
+combineversion=74x-root6
 # export SCRAM_ARCH=slc6_amd64_gcc530
+
 if [ ! -d "common/$therelease" ]; then
-    (cd common/ ; cmsrel "$therelease")
+    (cd common/
+     cmsrel "$therelease")
     mkdir "common/$therelease/src/HiggsAnalysis/"
-    git clone -b 74x-root6 https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git \
-        "common/$therelease/src/HiggsAnalysis/CombinedLimit/"
+    combine_path=
+    git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git "common/$therelease/src/HiggsAnalysis/CombinedLimit/"
+    (cd "common/$therelease/src/HiggsAnalysis/CombinedLimit/"
+     git checkout $combineversion)
 fi
 
 cd "common/$therelease/src"
 cmsenv
-cd "../../.."
+cd ../../..
 
 export LD_LIBRARY_PATH=$PWD/babymaking/batch:$LD_LIBRARY_PATH
 export PYTHONPATH=$PWD:$PYTHONPATH
