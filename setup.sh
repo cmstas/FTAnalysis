0de2524b163360499892ce377b007ca8c94a9c51
Caleb Fangmeier <caleb@fangmeier.tech>
2018-01-18 13:09:47 -0800

    Adds yaml-cpp for working w/ yaml configuration files

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ if [ ! -d "common/$therelease" ]; then
     (cd common/
      cmsrel "$therelease")
     mkdir "common/$therelease/src/HiggsAnalysis/"
-    combine_path=
     git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git "common/$therelease/src/HiggsAnalysis/CombinedLimit/"
     (cd "common/$therelease/src/HiggsAnalysis/CombinedLimit/"
      git checkout $combineversion)
@@ -16,10 +15,21 @@ cd "common/$therelease/src"
 cmsenv
 cd ../../..
 
-export LD_LIBRARY_PATH=$PWD/babymaking/batch:$LD_LIBRARY_PATH
-export PYTHONPATH=$PWD:$PYTHONPATH
-
 if [ "$1" = "build" ]; then
+    echo "Building CMSSW Release"
     (cd "common/$therelease/src" ; scram b -j 15)
+
+    echo "Building yaml-cpp"
+    (mkdir -p common/yaml-cpp/build
+     cd common/yaml-cpp/build
+     cmake .. -DBUILD_SHARED_LIBS=ON
+     make -j15)
+
+    echo "Building CORE"
     make -j15 -C common/CORE
 fi
+
+export ROOT_INCLUDE_PATH="$PWD/common/yaml-cpp/include:$ROOT_INCLUDE_PATH"
+
+export LD_LIBRARY_PATH="$PWD/babymaking/batch:$PWD/common/yaml-cpp/build/:$LD_LIBRARY_PATH"
+export PYTHONPATH="$PWD:$PYTHONPATH"
