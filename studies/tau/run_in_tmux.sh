b2f304de1b453ed4d0e61692b51781eb0278a384
Caleb Fangmeier <caleb@fangmeier.tech>
2018-01-18 15:10:02 -0800

    Updates run_in_tmux script for new configuration

--- studies/tau/run_in_tmux.sh
+++ studies/tau/run_in_tmux.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
 
-# events_max=0 -> all events
-events_max=1000000
-# events_max=100
-# events_max=0
-
-output_path="old_output/"
 
 echo "Compiling code"
 source /code/osgcode/cmssoft/cms/cmsset_default.sh
@@ -19,25 +13,25 @@ if [ "$?" -eq 0 ]; then
                             cd ~/FTAnalysis && \
                             source setup.sh && \
                             cd studies/tau/ && clear && \
-                            output_path=$output_path events_max=$events_max datasetIdx=0 root -b -q -l doAll.C"
+                            dataset=tttt root -b -q -l doAll.C"
 
     tmux split-window -h  "source /code/osgcode/cmssoft/cms/cmsset_default.sh && \
                            cd ~/FTAnalysis && \
                            source setup.sh && \
                            cd studies/tau/ && clear && \
-                           output_path=$output_path events_max=$events_max datasetIdx=1 root -b -q -l doAll.C"
+                           dataset=ttw root -b -q -l doAll.C"
 
     tmux split-window -h  "source /code/osgcode/cmssoft/cms/cmsset_default.sh && \
                            cd ~/FTAnalysis && \
                            source setup.sh && \
                            cd studies/tau/ && clear && \
-                           output_path=$output_path events_max=$events_max datasetIdx=2 root -b -q -l doAll.C"
+                           dataset=ttz root -b -q -l doAll.C"
 
     tmux split-window -h  "source /code/osgcode/cmssoft/cms/cmsset_default.sh && \
                            cd ~/FTAnalysis && \
                            source setup.sh && \
                            cd studies/tau/ && clear && \
-                           output_path=$output_path events_max=$events_max datasetIdx=3 root -b -q -l doAll.C"
+                           dataset=tth root -b -q -l doAll.C"
 
     tmux select-layout even-vertical
 fi
