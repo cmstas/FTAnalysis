43608efd89d3454e68a5fd29822b4f80ae4c5a92
Nick Amin <amin.nj@gmail.com>
2018-03-30 12:18:27 -0700

    2cr+16sr=18 bin scheme

--- analysis/misc/signal_regions.h
+++ analysis/misc/signal_regions.h
@@ -5,37 +5,54 @@ int signalRegionBDT(float disc){
     return 1+(int)(disc*getNsrsTTTTBDT());
 }
 
-// nominal
-int signalRegionTest(int njets, int nbtags, float met, float ht, float mt_min, int id1, int id2, float lep1pt, float lep2pt, int nleps, int isClass6){
-    if (lep1pt < 25.) return -1;
-    if (lep2pt < 20.) return -1;
-    if (njets < 2) return -1;
-    if (nbtags < 2) return -1;
-    if (ht < 300) return -1;
-    if (isClass6) return 1;
-    if (nleps == 2) {
-        if (nbtags == 2) {
-            if (njets <= 5) return 2;
-            if (njets == 6) return 3;
-            if (njets == 7) return 4;
-            if (njets >= 8) return 5;
-        } else if (nbtags == 3) {
-            if (njets == 5) return 6;
-            if (njets == 6) return 6;
-            if (njets >= 7) return 7;
-        } else if (nbtags >= 4) {
-            if (njets >= 5) return 8;
-        }
-    } else {
-        if (nbtags == 2) {
-            if (njets >= 5) return 9;
-        } else if (nbtags >= 3) {
-            if (njets >= 4) return 10;
-        }
-    }
-    return -1;
+float ptcut(int id){
+  return (abs(id) == 11) ? 15 : 10; 
 }
-int getNsrsTTTT() { return 10; }
+int baseline_region(int njets, int nbtags, float met, float ht, int id1, int id2, float lep1_pt, float lep2_pt, float metcut) {
+  //Kinematic cuts
+  if (lep1_pt < ptcut(id1)) return -1;
+  if (lep2_pt < ptcut(id2)) return -1;
+  if (njets < 2) return -1;
+  if (met < metcut) return -1;
+ 
+  //Return baseline region
+  if      (nbtags == 0) return 0;
+  else if (nbtags == 1) return 1;
+  else if (nbtags == 2) return 2;
+  else                  return 3;
+}
+
+// // nominal
+// int signalRegionTest(int njets, int nbtags, float met, float ht, float mt_min, int id1, int id2, float lep1pt, float lep2pt, int nleps, int isClass6){
+//     if (lep1pt < 25.) return -1;
+//     if (lep2pt < 20.) return -1;
+//     if (njets < 2) return -1;
+//     if (nbtags < 2) return -1;
+//     if (ht < 300) return -1;
+//     if (isClass6) return 1;
+//     if (nleps == 2) {
+//         if (nbtags == 2) {
+//             if (njets <= 5) return 2;
+//             if (njets == 6) return 3;
+//             if (njets == 7) return 4;
+//             if (njets >= 8) return 5;
+//         } else if (nbtags == 3) {
+//             if (njets == 5) return 6;
+//             if (njets == 6) return 6;
+//             if (njets >= 7) return 7;
+//         } else if (nbtags >= 4) {
+//             if (njets >= 5) return 8;
+//         }
+//     } else {
+//         if (nbtags == 2) {
+//             if (njets >= 5) return 9;
+//         } else if (nbtags >= 3) {
+//             if (njets >= 4) return 10;
+//         }
+//     }
+//     return -1;
+// }
+// int getNsrsTTTT() { return 10; }
 
 
 // // 3 bins
@@ -128,44 +145,44 @@ int getNsrsTTTT() { return 10; }
 // }
 // int getNsrsTTTT() { return 8; }
 
-// // 18 bins
-// int signalRegionTest(int njets, int nbtags, float met, float ht, float mt_min, int id1, int id2, float lep1pt, float lep2pt, int nleps, int isClass6){
-//     if (lep1pt < 25.) return -1;
-//     if (lep2pt < 20.) return -1;
-//     if (njets < 2) return -1;
-//     if (nbtags < 2) return -1;
-//     if (ht < 300) return -1;
-//     if (isClass6) return 1;
-//     if (nleps == 2) {
-//         if (nbtags == 2) {
-//             if (njets <= 5) return 2;
-//             if (njets == 6) return 3;
-//             if (njets == 7) return 4;
-//             if (njets >= 8) return 5;
-//         } else if (nbtags == 3) {
-//             if (njets <= 5) return 6;
-//             if (njets == 6) return 7;
-//             if (njets == 7) return 8;
-//             if (njets >= 8) return 9;
-//         } else if (nbtags >= 4) {
-//             if (njets <= 5) return 10;
-//             if (njets == 6) return 11;
-//             if (njets >= 7) return 12;
-//         }
-//     } else {
-//         if (nbtags == 2) {
-//             if (njets <= 5) return 13;
-//             if (njets == 6) return 14;
-//             if (njets >= 7) return 15;
-//         } else if (nbtags >= 3) {
-//             if (njets <= 4) return 16;
-//             if (njets == 5) return 17;
-//             if (njets >= 6) return 18;
-//         }
-//     }
-//     return -1;
-// }
-// int getNsrsTTTT() { return 18; }
+// 18 bins
+int signalRegionTest(int njets, int nbtags, float met, float ht, float mt_min, int id1, int id2, float lep1pt, float lep2pt, int nleps, int isClass6){
+    if (lep1pt < 25.) return -1;
+    if (lep2pt < 20.) return -1;
+    if (njets < 2) return -1;
+    if (nbtags < 2) return -1;
+    if (ht < 300) return -1;
+    if (isClass6) return 1;
+    if (nleps == 2) {
+        if (nbtags == 2) {
+            if (njets <= 5) return 2;
+            if (njets == 6) return 3;
+            if (njets == 7) return 4;
+            if (njets >= 8) return 5;
+        } else if (nbtags == 3) {
+            if (njets == 5) return 6;
+            if (njets == 6) return 7;
+            if (njets == 7) return 8;
+            if (njets >= 8) return 9;
+        } else if (nbtags >= 4) {
+            if (njets == 5) return 10;
+            if (njets == 6) return 11;
+            if (njets >= 7) return 12;
+        }
+    } else {
+        if (nbtags == 2) {
+            if (njets == 5) return 13;
+            if (njets == 6) return 14;
+            if (njets >= 7) return 15;
+        } else if (nbtags >= 3) {
+            if (njets == 4) return 16;
+            if (njets == 5) return 17;
+            if (njets >= 6) return 18;
+        }
+    }
+    return -1;
+}
+int getNsrsTTTT() { return 18; }
 
 
 #endif
