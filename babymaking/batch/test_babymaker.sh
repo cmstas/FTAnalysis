d228927e595c0ec7c6f7ca2f3aeb5ff937cdc4ad
Nick Amin <amin.nj@gmail.com>
2018-04-04 10:43:58 -0700

    update test samples

--- babymaking/batch/test_babymaker.sh
+++ babymaking/batch/test_babymaker.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 
 # Data
-
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016B_DoubleMuon_MINIAOD_23Sep2016-v3/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_1.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016C_DoubleMuon_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_2.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016D_DoubleMuon_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_3.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016E_DoubleMuon_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_4.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016F_DoubleMuon_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_5.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016G_DoubleMuon_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root  test_output_DATA_6.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016H_DoubleMuon_MINIAOD_PromptReco-v2/merged/V08-00-14/merged_ntuple_1.root test_output_DATA_7.root 500
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016F_MuonEG_MINIAOD_23Sep2016-v1/merged/V08-00-14/merged_ntuple_1.root      test_output_DATA_8.root 500
-
-# ./main.exe /hadoop/cms/store/group/snt/run2_data/Run2016C_MuonEG_MINIAOD_03Feb2017-v1/merged/V08-00-18/merged_ntuple_1.root  test_output_DATA_1.root 500
-
-
-# Signal
-# ./main.exe /hadoop/cms/store/group/snt/run2_25ns_80MiniAODv2_fastsim/SMS-T5qqqqVV_dM20_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/V08-00-09/merged_ntuple_1.root test_output_SIG_1.root 1000
+# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/DoubleEG_Run2017B-PromptReco-v2_MINIAOD_CMS4_V00-00-06/merged_ntuple_12.root test_output_Data_1.root 20000
+# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/DoubleEG_Run2017D-PromptReco-v1_MINIAOD_CMS4_V00-00-06/merged_ntuple_12.root test_output_Data_1.root 20000
+./main.exe /hadoop/cms/store/user/namin/ProjectMetis/MuonEG_Run2017D-PromptReco-v1_MINIAOD_CMS4_V00-00-06/merged_ntuple_6.root test_output_Data_1.root 100000
 
 # # MC
-# ./main.exe /hadoop/cms/store/group/snt/run2_moriond17/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/V08-00-16/merged_ntuple_1.root test_output_MC_1.root 1000
+# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/TTTT_TuneCP5_13TeV-amcatnlo-pythia8_RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1_MINIAODSIM_CMS4_V00-00-10_test/merged_ntuple_1.root test_output_MC_1.root 1000
 
 # wait
+
+echo $?
