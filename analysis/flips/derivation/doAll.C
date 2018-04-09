17cbddbfbc9b6d8b74ddb7185c8d6b2135d9f8c8
Nick Amin <amin.nj@gmail.com>
2018-04-09 14:04:31 -0700

    add ttbar to flips

--- analysis/flips/derivation/doAll.C
+++ analysis/flips/derivation/doAll.C
@@ -2,16 +2,24 @@
 
     gSystem->Exec("mkdir -p outputs");
 
-    gROOT->ProcessLine(".L ../misc/class_files/v8.02/SS.cc+"); 
+    gROOT->ProcessLine(".L ../../misc/class_files/v8.02/SS.cc+"); 
     gROOT->ProcessLine(".L ScanChain.C+");
 
-    TString tag = "v1.00_94x_baseline_v1";
+    TString tag = "v1.00_94x_baseline_withos_v3";
     TString basedir = Form("/nfs-7/userdata/namin/tupler_babies/merged/FT/%s/output/",tag.Data());
+
+    TChain *ch_tt = new TChain("t","tt");
+    ch_tt->Add(basedir+"TTBAR*.root");
+    flip(ch_tt);
 
     TChain *ch_dy = new TChain("t","dy");
     ch_dy->Add(basedir+"DY*.root");
-
     flip(ch_dy);
+
+    TChain *ch_both = new TChain("t","both");
+    ch_both->Add(basedir+"DY*.root");
+    ch_both->Add(basedir+"TTBAR*.root");
+    flip(ch_both);
 
 }
 
---
+++ analysis/flips/derivation/dump.py
@@ -0,0 +1,33 @@
+import ROOT as r
+
+def print_flip_rate(hist, do_errors=False):
+  print "float flipRate%s(float pt, float eta) {" % ("" if not do_errors else "Error")
+  for xbin in range(1,ratio.GetNbinsX()+1):
+      for ybin in range(1,ratio.GetNbinsY()+1):
+          val = ratio.GetBinContent(xbin,ybin)
+          if do_errors:
+              val = ratio.GetBinError(xbin,ybin)
+          if xbin != ratio.GetNbinsX():
+              print "   if (pt>={} && pt<{} && fabs(eta)>={:.3f} && fabs(eta)<{:.3f} ) return {};".format(
+                      ratio.GetXaxis().GetBinLowEdge(xbin),
+                      ratio.GetXaxis().GetBinUpEdge(xbin),
+                      ratio.GetYaxis().GetBinLowEdge(ybin),
+                      ratio.GetYaxis().GetBinUpEdge(ybin),
+                      val,
+                      )
+          else:
+              print "   if (pt>={} && fabs(eta)>={:.3f} && fabs(eta)<{:.3f} ) return {};".format(
+                      ratio.GetXaxis().GetBinLowEdge(xbin),
+                      ratio.GetYaxis().GetBinLowEdge(ybin),
+                      ratio.GetYaxis().GetBinUpEdge(ybin),
+                      val,
+                      )
+  print "   return 0.;"
+  print "}"
+
+if __name__ == "__main__":
+
+    f = r.TFile("outputs/histos_dy.root")
+    ratio = f.Get("ratio")
+    print_flip_rate(ratio, do_errors=False)
+    print_flip_rate(ratio, do_errors=True)
