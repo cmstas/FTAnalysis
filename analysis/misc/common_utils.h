528e75432f8db36757e7cead019dea0b8ac1c701
Nick Amin <amin.nj@gmail.com>
2018-04-17 09:40:21 -0700

    update mva cuts

--- analysis/misc/common_utils.h
+++ analysis/misc/common_utils.h
@@ -59,15 +59,27 @@ float mvacut(float A, float B, float C, float pt_) {
     return pt_>10 ? std::min(A, std::max(B,A + (B-A)/(ptmax-ptmin)*(pt_-ptmin))) : C; 
 }
 
+// C if pT<10, A if pT=10, B if pt>=25, lerp between A,B for pT in [10,25]
+float mvacut2017(float A, float B, float C, float pt_) {
+    if (pt_ < 10) return C;
+    else if (pt_ > 25) return B;
+    else {
+        return A + (B-A)/15.0f*(pt_-10.0f);
+    }
+}
+
 bool passIsolatedFO(int id, float eta, float disc, float pt) {
   if (abs(id)==13) return true;
   float aeta = fabs(eta);
   // if (aeta < 0.8) return mva > -0.155;
   // if ((aeta >= 0.8 && aeta <= 1.479)) return mva > -0.56;
   // if (aeta > 1.479) return mva > -0.76;
-    if (aeta < 0.8) return disc > mvacut(-0.86,-0.96,-0.3,pt);
-    if ((aeta >= 0.8 && aeta <= 1.479)) return disc > mvacut(-0.85,-0.96,-0.36,pt);
-    if (aeta > 1.479) return disc > mvacut(-0.81,-0.95,-0.63,pt);
+    // if (aeta < 0.8) return disc > mvacut(-0.86,-0.96,-0.3,pt);
+    // if ((aeta >= 0.8 && aeta <= 1.479)) return disc > mvacut(-0.85,-0.96,-0.36,pt);
+    // if (aeta > 1.479) return disc > mvacut(-0.81,-0.95,-0.63,pt);
+    if (aeta < 0.8) return disc > mvacut2017(-0.93,-0.887,-0.135,pt);
+    if ((aeta >= 0.8 && aeta <= 1.479)) return disc > mvacut2017(-0.93,-0.89,-0.417,pt);
+    if (aeta > 1.479) return disc > mvacut2017(-0.942,-0.91,-0.470,pt);
   return false;
 }
 
