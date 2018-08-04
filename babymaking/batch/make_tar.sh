9e44250bd9893b15c32611d6e8cc364fc16d7f56
Nick Amin <amin.nj@gmail.com>
2018-08-03 20:57:21 -0700

    exclude .git and xml for bdt which we never use to reduce file size to 11mb or so

--- babymaking/batch/make_tar.sh
+++ babymaking/batch/make_tar.sh
@@ -4,14 +4,16 @@ echo "Did you do 'make opt' instead of 'make'? It will be much faster"
 
 final_dir="inputs"
 files="
-package.tar.gz 
-main.exe 
-liblooperBatch.so 
-CORE 
-LinkDef_out_rdict.pcm 
+package.tar.gz
+main.exe
+liblooperBatch.so
+CORE
+LinkDef_out_rdict.pcm
 goodRunList
 "
+
 mkdir -p $final_dir
-tar -czf $files
+tar -cz --exclude='CORE/data' --exclude-vcs -f $files
 mv package.tar.gz  $final_dir
 echo "I put stuff into $final_dir!"
+
