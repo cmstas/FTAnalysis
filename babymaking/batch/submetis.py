67e6a08243b53e6baf773aaf5c94e83f2964072f
Nick Amin <amin.nj@gmail.com>
2018-08-06 00:34:38 -0700

    add master list of samples and add niceties to submetis

--- babymaking/batch/submetis.py
+++ babymaking/batch/submetis.py
@@ -1,3 +1,4 @@
+import sys
 import glob
 import os
 import time
@@ -15,45 +16,67 @@ def split_func(dsname):
     if any(x in dsname for x in [
         "/W","/Z","/TTJets","/DY","/ST",
         ]):
-        return 4
-    elif "Run201" in dsname:
         return 5
+    elif "Run201" in dsname:
+        return 6
     else:
         return 2
 
 if __name__ == "__main__":
 
-    sample_map = {
-            "/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"    : "TTBAR_PH",
-            "/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM": "TTTTnew",
-            "/TTTW_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"          : "TTTW",
-            }
+    from samples import data_2016, mc_2016, data_2017, mc_2017, data_2018, mc_2018
+
+    sample_map = [
+
+            ["/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"    , "TTBAR_PH"],
+            # ["/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM", "TTTTnew"],
+            ["/TTTW_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"          , "TTTW"],
+
+            ]
 
     tag = "v1_PMtest"
 
+    # FIXME: want to add 2016,2017,2018 to merged_dir eventually
     merged_dir = "/nfs-7/userdata/{}/tupler_babies/merged/FT/{}/output/".format(os.getenv("USER"),tag)
-    for dsname,shortname in sample_map.items():
-        task = CondorTask(
-                sample = SNTSample(
-                    dataset=dsname,
-                    # tag="CMS4_V09-04-13", # if not specified, get latest tag
-                    ),
-                files_per_output = split_func(dsname),
-                output_name = "output.root",
-                tag = tag,
-                condor_submit_params = {"use_xrootd":True},
-                cmssw_version = "CMSSW_9_2_8",
-                input_executable = "inputs/condor_executable_metis.sh",
-                tarfile = "inputs/package.tar.xz",
-                special_dir = "FTbabies/",
-        )
-        merge_task = LocalMergeTask(
-                input_filenames=task.get_outputs(),
-                output_filename="{}/{}.root".format(merged_dir,shortname)
-                )
-        if not task.complete():
-            task.process()
-        else:
-            if not merge_task.complete():
-                merge_task.process()
+    
+    for i in range(100):
+        ndone = 0
+        total_summary = {}
+        for dsname,shortname in sample_map:
+            task = CondorTask(
+                    sample = SNTSample(
+                        dataset=dsname,
+                        # tag="CMS4_V09-04-13", # if not specified, get latest tag
+                        ),
+                    files_per_output = split_func(dsname),
+                    output_name = "output.root",
+                    tag = tag,
+                    # condor_submit_params = {"use_xrootd":True},
+                    condor_submit_params = {"sites":"T2_US_UCSD"}, # I/O is hella faster
+                    cmssw_version = "CMSSW_9_2_8",
+                    input_executable = "inputs/condor_executable_metis.sh",
+                    tarfile = "inputs/package.tar.xz",
+                    special_dir = "FTbabies/",
+            )
+            merge_task = LocalMergeTask(
+                    input_filenames=task.get_outputs(),
+                    output_filename="{}/{}.root".format(merged_dir,shortname)
+                    )
+            if not task.complete():
+                task.process()
+            else:
+                if not merge_task.complete():
+                    merge_task.process()
+                else:
+                    ndone += 1
+                    
+            total_summary[dsname] = task.get_task_summary()
+
+        StatsParser(data=total_summary, webdir="~/public_html/dump/FTmetis/").do()
+
+        if ndone == len(sample_map):
+            print "All done!"
+            sys.exit()
+
+        time.sleep(10*60)
 
