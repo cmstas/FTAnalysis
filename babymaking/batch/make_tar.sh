6a8a03b7e1dd486184e6d3bc9a780d8b63d57b0c
Nick Amin <amin.nj@gmail.com>
2018-09-01 22:36:55 -0700

    smaller tar files

--- babymaking/batch/make_tar.sh
+++ babymaking/batch/make_tar.sh
@@ -14,7 +14,24 @@ goodRunList
 
 mkdir -p $final_dir
 # tar -cz --exclude='CORE/data' --exclude='*.cc' --exclude='*.h' --exclude='*.C' --exclude '*.cxx' --exclude-vcs -f $files
-XZ_OPT=-9 tar -Jc --exclude='CORE/data' --exclude='*.cc' --exclude='*.h' --exclude='*.C' --exclude '*.cxx' --exclude-vcs -f $files
+XZ_OPT=-9 tar -Jc \
+    --exclude='CORE/data' \
+    --exclude='CORE/CMS3_CORE*' \
+    --exclude='CORE/Tools/dorky' \
+    --exclude='CORE/Tools/MT2' \
+    --exclude='CORE/Tools/classfiles' \
+    --exclude='*run2_50ns*' \
+    --exclude='*AK8PF*.txt' \
+    --exclude='*UncertaintySources*txt' \
+    --exclude='*Summer16_23Sep2016*V3_*txt' \
+    --exclude='*PFPuppi*.txt' \
+    --exclude='*.cc' \
+    --exclude='*.h' \
+    --exclude='*.C' \
+    --exclude '*.cxx' \
+    --exclude-vcs \
+    -f $files
+ls -lh package.tar.xz
 mv package.tar.xz  $final_dir
 echo "I put stuff into $final_dir!"
 
