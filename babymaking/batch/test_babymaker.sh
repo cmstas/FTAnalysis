80ad3e75ed0c8a78fa798e0dc4fa15d6dfa84cd2
Nick Amin <amin.nj@gmail.com>
2018-09-10 14:08:22 -0700

    better, parallel tester. not yet in python, though.

--- babymaking/batch/test_babymaker.sh
+++ babymaking/batch/test_babymaker.sh
@@ -1,29 +1,53 @@
 #!/usr/bin/env bash
 
-# Data
-# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/DoubleEG_Run2017B-PromptReco-v2_MINIAOD_CMS4_V00-00-06/merged_ntuple_12.root test_output_Data_1.root 20000
-# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/DoubleEG_Run2017D-PromptReco-v1_MINIAOD_CMS4_V00-00-06/merged_ntuple_12.root test_output_Data_1.root 20000
-# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/MuonEG_Run2017D-PromptReco-v1_MINIAOD_CMS4_V00-00-06/merged_ntuple_6.root test_output_Data_1.root 100000
+function get_dataset() {
+    which=$1
+    sampstr=$2
+    python -c 'from samples import '${which}' ; y = [x for x in '${which}' if "'${sampstr}'" in x[0]]; print y[len(y)//2][0]';
+}
 
-# MC
-# ./main.exe /hadoop/cms/store/user/namin/ProjectMetis/TTTT_TuneCP5_13TeV-amcatnlo-pythia8_RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v1_MINIAODSIM_CMS4_V00-00-10_test/merged_ntuple_1.root test_output_MC_1.root 10000
-# ./main.exe /hadoop/cms/store/group/snt/run2_mc2017//TTTT_TuneCP5_13TeV-amcatnlo-pythia8_RunIIFall17MiniAODv2-PU2017_pilot_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V09-04-13/merged_ntuple_1.root test_output_MC_2.root 10000
-# ./main.exe /hadoop/cms/store/group/snt/run2_mc2017//DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V09-04-13/merged_ntuple_1.root test_output_DY_1.root 1000
+function get_random_file() {
+    datasetname=$1;
+    dir=$(dis_client.py -t snt "${datasetname} | grep location" | head -1);
+    last=$(ls -1 ${dir}/*.root | sort | shuf --random-source=<(yes) | head -n 1)
+    echo $last
+}
 
+function get_file() {
+    get_random_file $(get_dataset $1 "$2")
+}
 
+function check() {
+    fname=$1
+    badstr="\e[91mx\e[0m"
+    goodstr="\e[92mo\e[0m"
+    pgood=$([ -n "$(grep "Processed [1-9]" ${fname})" ] && echo $goodstr || echo $badstr);
+    sgood=$([ -n "$(grep "Stored [1-9]" ${fname})" ] && echo $goodstr || echo $badstr);
+    echo -e "$pgood$sgood $fname"
+}
 
-# # synch with 2017 --
-# # /home/users/namin/2018/fourtop/94x/FTAnalysis/babymaking/batch/test_babymaker.sh
-# ./main.exe /hadoop/cms/store/group/snt/run2_mc2017_test//TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_test_CMS4_V09-04-13/merged_ntuple_1.root test_synch_2017_tttt_1.root 20000
-# ./main.exe /hadoop/cms/store/group/snt/run2_mc2017//DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_RunIIFall17MiniAODv2-PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1_MINIAODSIM_CMS4_V09-04-13/merged_ntuple_1.root test_synch_2017_DY_1.root 100000
-# ./main.exe /hadoop/cms/store/group/snt/run2_data2017//DoubleEG_Run2017F-31Mar2018-v1_MINIAOD_CMS4_V09-04-12/merged_ntuple_100.root test_synch_2017_DataDoubleEG_1.root 100000
+function run() {
+    echo Running ./main.exe $(get_file $1 $2) $3 10000 $4
+    ./main.exe $(get_file $1 $2) $3 10000 >& $4
+    check $4
+}
 
-# # synch with 2016 --
-# # /home/users/namin/2018/fourtop/80x/FTAnalysis/babymaking/batch/test_babymaker.sh
-# ./main.exe /hadoop/cms/store/group/snt/run2_data2016//MuonEG_Run2016C-03Feb2017-v1_MINIAOD_CMS4_V00-00-02_data2016/merged_ntuple_42.root test_synch_2016_DataMuonEGC_1.root 100000
-./main.exe /hadoop/cms/store/user/namin/run2_moriond17_cms4/ProjectMetis/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1_MINIAODSIM_CMS4_V00-00-02_2017Sep27/merged_ntuple_4.root test_synch_2016_WZ_1.root 100000
-# ./main.exe /hadoop/cms/store/user/namin/run2_moriond17_cms4/ProjectMetis/TTTT_TuneCUETP8M2T4_13TeV-amcatnlo-pythia8_RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1_MINIAODSIM_CMS4_V00-00-02_2017Sep27/merged_ntuple_1.root test_synch_2016_tttt_1.root 20000
+outputdir=outputs_test/
+mkdir -p ${outputdir}
+rm ${outputdir}/log*.txt
 
-# wait
+# echo $(get_random_file $(get_dataset mc_2017 "/TTWJets"))
+# return
 
-echo $?
+run mc_2016   "/TTWJets"    ${outputdir}/test_2016_ttw.root    ${outputdir}/log_2016_ttw.txt    &
+run mc_2016   "/TTTT"       ${outputdir}/test_2016_tttt.root   ${outputdir}/log_2016_tttt.txt   &
+run data_2016 "/DoubleEG"   ${outputdir}/test_2016_dataee.root ${outputdir}/log_2016_dataee.txt &
+run data_2016 "/DoubleMuon" ${outputdir}/test_2016_datamm.root ${outputdir}/log_2016_datamm.txt &
+run mc_2017   "/TTWJets"    ${outputdir}/test_2017_ttw.root    ${outputdir}/log_2017_ttw.txt    &
+run mc_2017   "/TTTT"       ${outputdir}/test_2017_tttt.root   ${outputdir}/log_2017_tttt.txt   &
+run data_2017 "/DoubleEG"   ${outputdir}/test_2017_dataee.root ${outputdir}/log_2017_dataee.txt &
+run data_2017 "/DoubleMuon" ${outputdir}/test_2017_datamm.root ${outputdir}/log_2017_datamm.txt &
+run data_2018 "/EGamma"     ${outputdir}/test_2018_dataee.root ${outputdir}/log_2018_dataee.txt &
+run data_2018 "/DoubleMuon" ${outputdir}/test_2018_datamm.root ${outputdir}/log_2018_datamm.txt &
+
+wait
